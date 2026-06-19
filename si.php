0f432418ef82e4ac9bac3d513742d59079c3f29f
PirxDanford <pirx@verschiedenesachen.de>
2026-06-19 08:38:47 +0200

    feat(cli): add user listing and admin bootstrap commands

--- si.php
+++ si.php
@@ -3,8 +3,10 @@
 declare(strict_types=1);
 
 use SparkInsight\Command\CheckEnvironmentCommand;
+use SparkInsight\Command\BootstrapAdminCommand;
 use SparkInsight\Command\GenerateInvitationCommand;
 use SparkInsight\Command\InitializeDbCommand;
+use SparkInsight\Command\ListUsersCommand;
 use SparkInsight\Command\MigrateDbCommand;
 use SparkInsight\Command\ScrivenerImportCommand;
 use Symfony\Component\Console\Application;
@@ -14,6 +16,8 @@ require __DIR__ . '/vendor/autoload.php';
 $app = new Application('SparkInsight CLI', '1.0.0');
 $app->add(new CheckEnvironmentCommand());
 $app->add(new InitializeDbCommand());
+$app->add(new BootstrapAdminCommand());
+$app->add(new ListUsersCommand());
 $app->add(new MigrateDbCommand());
 $app->add(new GenerateInvitationCommand());
 $app->add(new ScrivenerImportCommand());
